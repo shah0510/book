8e2bca5a3d6f420844eb7af11d4a23e9cc48eb54
shah0510 <meetshah0510@gmail.com>
2024-08-24 23:47:00 +0530

    fir

--- ashish.js
+++ ashish.js
@@ -13,10 +13,23 @@ function movePage(e, page) {
     currentPage += 2;
     toggleClass(e, "left-side");
     toggleClass(e.nextElementSibling, "left-side");
-  } else if ((page = currentPage - 1)) {
+  } else if (page == currentPage - 1) {
     currentPage -= 2;
     toggleClass(e, "left-side");
     toggleClass(e.previousElementSibling, "left-side");
   }
 }
 
+// Add event listeners for both click and touch events
+document.addEventListener('DOMContentLoaded', function() {
+  const pages = document.querySelectorAll('.page');
+  pages.forEach((page, index) => {
+    page.addEventListener('click', function(e) {
+      movePage(this, index + 1);
+    });
+    page.addEventListener('touchend', function(e) {
+      e.preventDefault();
+      movePage(this, index + 1);
+    });
+  });
+});
